19c6b5ddea4b806145bcced6ce51df44f43f15c2
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-09-29 17:55:13 -0400

    Create sql_joins.sql

---
+++ sql_joins.sql
@@ -0,0 +1,4 @@
+-- Three Way Exercise
+-- Hmmm...I wonder if any authors are writing their own reviews for books! You are working with a database of books, authors, and reviews
+
+-- Write a query that will return the title of each book, along with the name of the author, and the rating of a review.  Only show rows where the author of the book is also the author of the review.
